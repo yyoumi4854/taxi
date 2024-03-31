3cc0a7cc391655fa59f333eb6cefa69a22bbb78f
유민지 <yyoumi4854@naver.com>
2024-03-31 23:10:04 +0900

    firebase analytics, crashlytics 세팅, 로고변경

--- app/App.tsx
+++ app/App.tsx
@@ -2,11 +2,17 @@
 import React from 'react';
 import {NavigationContainer, DefaultTheme} from '@react-navigation/native';
 import StackNavigator from './navigation/StackNavigator';
+// import crashlytics from '@react-native-firebase/crashlytics';
 
 // library
 import {RecoilRoot} from 'recoil';
 
 const App = () => {
+  // 강제로 crash 로그를 발생시켜 확인.
+  // useEffect(() => {
+  //   crashlytics().crash();
+  // }, []);
+
   // 네비게이션 테마
   const customTheme = {
     ...DefaultTheme,
